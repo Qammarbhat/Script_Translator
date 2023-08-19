adb4deca63097d191595bd3ed70df73f8c79372a
Qammarbhat <qammarbhat@bgsbu.ac.in>
2023-08-19 20:07:15 +0530

    Optimised Prompt

--- translator.py
+++ translator.py
@@ -1,18 +1,23 @@
 from langchain import PromptTemplate
 import os
 from langchain.llms import OpenAI
-os.environ["OPENAI_API_KEY"] = 'YOUR API KEY'
+os.environ["OPENAI_API_KEY"] = "Your API KAY"
 
 # creating LLM (will try OpenAI, modelname= gpt-3.5-turbo)
 llm = OpenAI(model_name = "gpt-3.5-turbo")
 
 def text_translator(input_text, input_target_language, input_country_names):
     translate_prompt = PromptTemplate(
-        input_variables = ["input_text", "target_language", "country_names"],
-        template = """"
-        You are a helpful Translator. Translate the following demo script into {target_language} and use names from {country_names}:
-        {input_text}
-        """
+    input_variables = ["input_text", "target_language", "country_names"],
+    template = """
+    You are an advanced translator specializing in localizing scripts. Your task is to translate the following script into {target_language} while replacing any names with culturally appropriate names for {country_names}. Make sure to maintain the context and tone of the script.
+
+    Here is the script for localization:
+
+    {input_text}
+
+    As you replace names, consider the cultural and linguistic context of {country_names}. Deliver a translation that resonates with the audience in {country_names}.
+    """
     )
     # translated_text = llm(translate_prompt.format(text = input_text, target_language = input_target_language))
     # using chunks (Ran into token length issue, max tokens = 4096)
